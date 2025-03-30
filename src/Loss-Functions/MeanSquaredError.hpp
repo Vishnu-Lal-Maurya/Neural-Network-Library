db2e22cd95596484fcb4e58a69eac7904b60612b
ramudevasani <devasani.ramu.cse22@itbhu.ac.in>
2025-03-30 17:34:38 +0530

    Updated derivative for MSE

--- src/Loss-Functions/MeanSquaredError.hpp
+++ src/Loss-Functions/MeanSquaredError.hpp
@@ -11,20 +11,26 @@ namespace NN{
         double computeCost(const row& yActual, const row& yPredicted) override {
 
             double result {0.0};
-            int length = static_cast<int>(yActual.size());
+            int length { static_cast<int>(yActual.size()) };
 
             for(int i{0}; i<length; ++i){
                 std::size_t x { static_cast<std::size_t>(i) };
                 result += (yActual[x] - yPredicted[x]) * (yActual[x] - yPredicted[x]);
             }
-            result /= length;
+            result /= (2 * length);
             
             return result;
         }
 
-        double derivative(const row& yActual, const row& yPredicted) override {
+        row derivative(const row& yActual, const row& yPredicted) override {
 
-            double result {0.0};
+            int length { static_cast<int>(yActual.size()) };
+            row result(length);
+
+            for(int i{0}; i<length; ++i){
+                std::size_t x { static_cast<std::size_t>(i) };
+                result[x] = (yPredicted[x] - yActual[x]) / length;
+            }
 
             return result;
         }

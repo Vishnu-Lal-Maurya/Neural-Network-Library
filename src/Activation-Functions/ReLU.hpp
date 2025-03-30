d1071528662ce205fd5c4046258eb8d4b42501dd
ramudevasani <devasani.ramu.cse22@itbhu.ac.in>
2025-03-30 15:42:12 +0530

    added some code in ReLU

--- src/Activation-Functions/ReLU.hpp
+++ src/Activation-Functions/ReLU.hpp
@@ -1,5 +1,18 @@
 #ifndef RELU_H
 #define RELU_H
 
+#include "ActivationFunction.hpp"
+
+namespace NN{
+    class ReLU final: public ActivationFunction
+    {
+    public:
+        NN::row activate(NN::row& input){
+            // int length = input.size();
+            // NN::row result(length, 0);
+        };
+    };
+};
+
 
 #endif

3eb54dde05d723e20e867a35d6ac68154b821026
Vishnu-Lal-Maurya <vishnulal.maurya.cse22@itbhu.ac.in>
2025-03-30 16:40:18 +0530

    updated

--- src/aliases.hpp
+++ src/aliases.hpp
@@ -7,10 +7,10 @@ namespace NN{
     using row = std::vector<double>;
     using matrix = std::vector<std::vector<double>>;
 
-    std::ostream& operator<<(std::ostream& out, row& v){
+    std::ostream& operator<<(std::ostream& out,const row& v){
         out << '[';
-        int length { static_cast<int>(v.size()) };
-        for(int i=0; i<length; ++i){
+        std::size_t length {v.size()};
+        for(std::size_t i{0}; i<length; ++i){
             out << v[i];
             if(i!=length-1) out << ", ";
         }

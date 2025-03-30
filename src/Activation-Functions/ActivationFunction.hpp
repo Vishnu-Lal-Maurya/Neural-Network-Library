aa763748af802ed2bd0d46e7cfa0cf9e26c849df
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2025-03-30 15:31:27 +0530

    Adding globad typedef

--- src/Activation-Functions/ActivationFunction.hpp
+++ src/Activation-Functions/ActivationFunction.hpp
@@ -1,4 +1,14 @@
 #ifndef ACTIVATION_H
 #define ACTIVATION_H
 
+#include "../aliases.hpp"
+
+class ActivationFunction
+{
+public:
+    virtual NN::row activate(NN::row& input) = 0;
+    virtual NN::row derivate(NN::row& input) = 0;
+};
+
+
 #endif
---
+++ src/aliases.hpp
@@ -0,0 +1,11 @@
+#ifndef ALIASES_H
+#define ALIASES_H
+
+#include <vector>
+
+namespace NN{
+    using row = std::vector<double>;
+    using matrix = std::vector<std::vector<double>>;
+};
+
+#endif

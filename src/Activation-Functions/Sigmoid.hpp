8d1a060a375c6a0b7b689beeda0f0dadc03eee07
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2025-03-30 15:58:38 +0530

    Adding sigmoid

--- src/Activation-Functions/Sigmoid.hpp
+++ src/Activation-Functions/Sigmoid.hpp
@@ -1,5 +1,39 @@
 #ifndef SIGMOID_H
 #define SIGMOID_H
 
+#include "ActivationFunction.hpp"
+#include <cmath>
 
-#endif SIGMOID_H
+namespace NN{
+    class Sigmoid final: public ActivationFunction{
+    public:
+        row activate(row& input) override{
+            int length { static_cast<int>(input.size()) };
+            row result(length);
+
+            for(int i{0}; i<length; ++i){
+                result[i] = eval(input[i]);
+            }
+
+            return std::move(result);
+        }
+
+        row derivate(row& input) override{
+            int length { static_cast<int>(input.size()) };
+            row result(length);
+
+            for(int i{0}; i<length; ++i){
+                result[i] = (eval(input[i]) * (1.0 - eval(input[i])));
+            }
+
+            return std::move(result);
+        }
+
+    private:
+        double eval(double x){
+            return 1.0/(1.0 + exp(-x));
+        }
+    };
+}
+
+#endif 

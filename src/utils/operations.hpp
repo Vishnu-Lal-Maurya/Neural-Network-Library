a86cb45fe081351f96558cb2b9d14fabbd63e089
fireheart17 <iampiyushgoyal17@gmail.com>
2025-03-30 17:59:26 +0530

    fix

--- src/utils/operations.hpp
+++ src/utils/operations.hpp
@@ -6,30 +6,30 @@
 
 namespace NN{
 
-    void assertLength(const row& v1, const row& v2){
+    void assertDimensions(const row& v1, const row& v2){
         std::size_t length1{ v1.size() };
         std::size_t length2{ v2.size() };
-        assert(length1==length2 && "Lenghts Don't match");
+        assert(length1 == length2 && "Lengths Don't match");
     }
 
-    void assertLength(const matrix& v1, const matrix& v2){
+    void assertDimensions(const matrix& v1, const matrix& v2){
         std::size_t rows1{ v1.size() };
         std::size_t rows2{ v2.size() };
-        assert(rows1==rows2 && "Dimensions must match");
-        assert(rows1!=0 && "Dimension should be non-zero");
+        assert(rows1 == rows2 && "Dimensions must match");
+        assert(rows1 && "Dimension should be non-zero");
 
         std::size_t col1{ v1[0].size() };
         std::size_t col2{ v2[0].size() };
-        assert(rows1==rows2 && "Dimensions must match");
+        assert(rows1 == rows2 && "Dimensions must match");
     }
 
     row elementWiseRowOperations(const row& v1, const row& v2, char op){
-        assertLength(v1, v2);
+        assertDimensions(v1, v2);
 
         std::size_t length{ v1.size() };
         row result(length);
 
-        for(int i=0; i<length; ++i){
+        for(int i = 0; i < length; ++i){
             switch (op)
             {
             case '+':
@@ -42,6 +42,7 @@ namespace NN{
                 result[toUZ(i)] = v1[toUZ(i)] * v2[toUZ(i)];
                 break;
             case '/':
+                assert(v2[toUZ(i)] && "divide by zero");
                 result[toUZ(i)] = v1[toUZ(i)] / v2[toUZ(i)];
                 break;
             }
@@ -50,27 +51,28 @@ namespace NN{
     }
 
     matrix elementWiseMatrixOperations(const matrix& v1, const matrix& v2, char op){
-        assertLength(v1,v2);
+        assertDimensions(v1,v2);
         std::size_t numRows{ v1.size() };
         std::size_t numCols{ v1[0].size() };
 
         matrix result(numRows, row(numCols));
-        for(int i=0; i<numRows; ++i){
-            for(int j=0; j<numCols; ++j){
+        for(int i = 0; i < numRows; ++i){
+            for(int j = 0; j < numCols; ++j){
                 int val{};
                 switch (op)
                 {
                 case '+':
-                    val = v1[toUZ(i)][toUZ(j)] + v1[toUZ(i)][toUZ(j)];
+                    val = v1[toUZ(i)][toUZ(j)] + v2[toUZ(i)][toUZ(j)];
                     break;
                 case '-':
-                    val = v1[toUZ(i)][toUZ(j)] - v1[toUZ(i)][toUZ(j)];
+                    val = v1[toUZ(i)][toUZ(j)] - v2[toUZ(i)][toUZ(j)];
                     break;
                 case '*':
-                    val = v1[toUZ(i)][toUZ(j)] * v1[toUZ(i)][toUZ(j)];
+                    val = v1[toUZ(i)][toUZ(j)] * v2[toUZ(i)][toUZ(j)];
                     break; 
                 case '/':
-                    val = v1[toUZ(i)][toUZ(j)] / v1[toUZ(i)][toUZ(j)];
+                    assert(v2[toUZ(i)][toUZ(j)] && "divide by zero");
+                    val = v1[toUZ(i)][toUZ(j)] / v2[toUZ(i)][toUZ(j)];
                     break; 
                 }
                 result[toUZ(i)][toUZ(j)] = val;

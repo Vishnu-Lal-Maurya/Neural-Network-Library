774cbb842d800194c8361e068394e797c31c9111
ramudevasani <devasani.ramu.cse22@itbhu.ac.in>
2025-03-30 16:11:33 +0530

    Tests passed for ReLU

--- src/Activation-Functions/test.cpp
+++ src/Activation-Functions/test.cpp
@@ -4,10 +4,29 @@
 
 int main(){
     NN::Sigmoid sigmoid;
+    NN::ReLU relu;
+
     NN::row input{1.0, 2.0, 0.0, -1.0};
+
     NN::row output{ sigmoid.activate(input) };
+
+    NN::row activationOutput{ relu.activate(input) };
+    NN::row derivativeOutput{ relu.derivate(input) };
+
     for(auto x: output){
         std::cout << x << ' ';
     }
     std::cout << '\n';
+
+    std::cout << "activation output = ";
+    for(auto x: activationOutput){
+        std::cout << x << ' ';
+    }
+    std::cout << '\n';
+    std::cout << "derivate output = ";
+    for(auto x: derivativeOutput){
+        std::cout << x << ' ';
+    }
+    std::cout << '\n';
+
 }

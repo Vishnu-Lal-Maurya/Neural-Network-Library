b2ad6cf4da52f1613e16e06065c06b649d2af512
Vishnu-Lal-Maurya <vishnulal.maurya.cse22@itbhu.ac.in>
2025-03-30 16:50:15 +0530

    updated

--- src/Loss-Functions/LossFunction.hpp
+++ src/Loss-Functions/LossFunction.hpp
@@ -1,6 +1,15 @@
 #ifndef LOSSFUNCTION_H
 #define LOSSFUNCTION_H
+#include "../aliases.hpp"
 
 
+namespace NN{
+   class LossFunction
+   {
+   public:
+       virtual row computeCost(const row& input) = 0;
+       virtual row derivative(const row& input) = 0;
+   };
+}
 
 #endif

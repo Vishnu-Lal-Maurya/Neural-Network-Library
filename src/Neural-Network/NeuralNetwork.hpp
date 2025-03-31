c063b6ff205c806d549294df703870b3b4279cf5
Bhanu-Verma <bhanu.verma.cse22@itbhu.ac.in>
2025-03-31 12:42:15 +0530

    Added train in neural network

--- src/Neural-Network/NeuralNetwork.hpp
+++ src/Neural-Network/NeuralNetwork.hpp
@@ -25,18 +25,40 @@ namespace NN{
          m_layers.push_back(NN::Layer{prevNumOfNodes,currNumOfNodes,activationFunction});
       }  
 
-      void train(int epochs,row& input){
-         while(epochs--){
-            row output { forward(input) };
-            for(auto ele: output){
-               std::cout << ele << ' ';
+      void train(matrix& xTrain, row& yTrain, int epochs, double learningRate, const LossFunction& lossFunction){
+         assert((xTrain.size() == yTrain.size()) && "x_train and y_train sizes don't match");
+
+         
+         // Run the epochs -
+         for(int ep{1}; ep<=epochs; ++ep){
+            double totalLoss{ 0.0 };
+            for(int i{0}; i<xTrain.size(); ++i){
+
+               row output { forward(xTrain[i]) };
+               
+               // Doing one hot encoding !(if last layer has 1 output node)'
+               row actualOutput(m_layers.back().getOutputSize(),0.0);
+               if(m_layers.back().getOutputSize()!=1){
+                  int index{ static_cast<int>(yTrain[toUZ(i)]) };
+                  actualOutput[toUZ(index)] = 1.0;
+               }  
+               else{
+                  actualOutput[0] = yTrain[toUZ(i)];
+               }
+               totalLoss += lossFunction.computeCost(actualOutput, output);
+               
+               // @todo
+               // implement backward propagation
+               backward();
             }
-            std::cout << '\n';
-            backward();
+
+            double avgLoss { totalLoss/(static_cast<double>(yTrain.size())) };
+            std::cout << "Average loss after epoch " << ep << ": " << avgLoss << '\n';
+            
          }
       }
       
-      private:
+   private:
       
       std::vector<NN::Layer> m_layers{};
       int m_inputDim{};
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,29 @@
 #include "Neural-Network/NeuralNetwork.hpp"
 #include "Activation-Functions/ReLU.hpp"
 #include "Activation-Functions/Softmax.hpp"
+#include "./aliases.hpp"
+#include "./Loss-Functions/CategoricalCrossEntropy.hpp"
 
 int main(){
-    int numOfInputNodes{5};
+    int numOfInputNodes{2};
     NN:: NeuralNetwork neuralNet{numOfInputNodes};
-    neuralNet.addLayer(10,NN::ReLU{});
-    neuralNet.addLayer(10,NN::ReLU{});
-    neuralNet.addLayer(5,NN::Softmax{});
+    neuralNet.addLayer(8,NN::ReLU{});
+    // neuralNet.addLayer(10,NN::ReLU{});
+    neuralNet.addLayer(2,NN::Softmax{});
     int epochs{3};
-    NN::row input(numOfInputNodes,1.0);
+
+    NN::matrix xtrain{
+        {0,0},
+        {0,1},
+        {1,0},
+        {10,10},
+        {10,11},
+        {11,10}
+    };
+
+    NN::row ytrain{0,0,0,1,1,1};
     
-    neuralNet.train(epochs, input);
+    neuralNet.train(xtrain, ytrain, epochs, 0.1, NN::CategoricalCrossEntropy{});
 
     return 0;
 }
